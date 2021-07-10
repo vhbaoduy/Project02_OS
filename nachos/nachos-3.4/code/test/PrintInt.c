45e4b561f2152fa2ae536bef0bd84274442c870c
tranngoclam16 <63225101+tranngoclam16@users.noreply.github.com>
2021-07-11 00:56:53 +0700

    ReadInt and PrintInt

---
+++ nachos/nachos-3.4/code/test/PrintInt.c
@@ -0,0 +1,11 @@
+#include "syscall.h"
+#include "copyright.h"
+int main()
+{
+	int n;
+	//PrintString("Nhap 1 so:");
+	n = ReadInt();
+	//PrintString("So vua nhap:");
+	PrintInt(n); // Test the PrintInt function: print 100 to console.
+	return 0;
+}
